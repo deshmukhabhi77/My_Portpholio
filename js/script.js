b2ef65ee1439ab47d989b960f0851cf51acc477d
deshmukhabhi77 <159116148+deshmukhabhi77@users.noreply.github.com>
2025-05-24 16:07:55 +0530

    Add all project files

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Typing Animation
+var typed=new Typed(".typing",{
+    strings:["","Python Developer","Data Analyst","Data Scientist","Machine Learning Engineer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
